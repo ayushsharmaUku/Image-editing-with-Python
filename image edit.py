3e5868070c5d1ba0dd2f73f50ab0e6ae273c888c
ayushsharmaUku <147406035+ayushsharmaUku@users.noreply.github.com>
2024-02-06 20:22:17 +0530

    Add files via upload

---
+++ image edit.py
@@ -0,0 +1,14 @@
+pip install pillow
+from PIL import Image
+modiji = Image.open(r'C:\Users\ayush\Downloads\modiji.jpg')
+modiji.size
+king = Image.open(r"C:\Users\ayush\Downloads\king.jpg")
+king.size
+king = king.crop((0,0,1200,900))
+king.size
+king = king.rotate(180)
+modiji.putalpha(250)
+modiji
+king = king.resize((1200,450))
+modiji.paste(im=king,box=(0,450))
+modiji
